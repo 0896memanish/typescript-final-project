3121d7b0051b2730d36777cb9ac8e256e73d7fc9
Manish Kumar Singh <0896memanish@gmail.com>
2025-02-08 09:21:37 +0530

    Some improvements

--- src/components/Sessions/UpcomingSessions.tsx
+++ src/components/Sessions/UpcomingSessions.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { ReactNode, useEffect, useRef } from "react";
 import { useSessionContext } from "../../store/sessions-context";
 import Button from "../UI/Button";
 import Modal, { ModalRefProps } from "../UI/Modal";
@@ -16,18 +16,38 @@ export default function UpcomingSessions({ onClose }: UpcomingSessionsProps) {
       modalRef.current.open();
     }
   }, []);
+  let component: ReactNode;
+
+  if (ctxValue.upcomingSessions.length === 0) {
+    component = (
+      <article className="upcoming-session">
+        <div>
+          <p>You haven't booked any sessions yet!</p>
+        </div>
+        <p style={{ margin: "-0rem 0" }}>
+          <Button onClick={() => onClose()}>Cancel</Button>
+        </p>
+      </article>
+    );
+  } else {
+    component = (
+      <>
+        <ul>
+          {ctxValue.upcomingSessions.map((session) => (
+            <li key={session.id}>
+              <UpcomingSession session={session}></UpcomingSession>
+            </li>
+          ))}
+        </ul>
+        <p className="actions">
+          <Button onClick={onClose}>Close</Button>
+        </p>
+      </>
+    );
+  }
   return (
     <Modal ref={modalRef} onClose={onClose}>
-      <ul>
-        {ctxValue.upcomingSessions.map((session) => (
-          <li key={session.id}>
-            <UpcomingSession session={session}></UpcomingSession>
-          </li>
-        ))}
-      </ul>
-      <p className="actions">
-        <Button onClick={onClose}>Close</Button>
-      </p>
+      {component}
     </Modal>
   );
 }
